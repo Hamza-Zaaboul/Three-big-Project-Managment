17c417fea18fd0c56c62ed1e755d878d67022744
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-02-01 11:13:34 +0100

    BigProject OOP Management

---
+++ src/Experience/World/World.js
@@ -0,0 +1,33 @@
+import Experience from "../Experience"
+import Environment from "./Environment"
+import * as THREE from 'three'
+import Floor from "./Floor"
+import Fox from "./Fox"
+export default class World {
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+
+        this.resources.on('ready', () => {
+            //Setup Light
+            this.floor = new Floor()
+            this.fox = new Fox()
+            this.environment = new Environment()
+         
+        })
+
+
+
+
+    }
+
+    update()
+    {
+        if(this.fox)
+        {
+            this.fox.update()
+        }
+    }
+}
